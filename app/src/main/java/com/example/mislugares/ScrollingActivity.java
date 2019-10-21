ca07961393b9700f250182b28d84915c1427cbc3
Christian <christianDellarosa98@gmail.com>
2019-10-21 13:27:16 -0300

    Agregando estilos y temas

--- app/src/main/java/com/example/mislugares/ScrollingActivity.java
+++ app/src/main/java/com/example/mislugares/ScrollingActivity.java
@@ -1,13 +1,10 @@
 package com.example.mislugares;
 
 import android.os.Bundle;
-
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
-
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
-
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
